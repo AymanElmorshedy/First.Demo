0f1cf8e86eb41d063a9012fe5559c9421148818a
ayman <aymanehab2002@gmail.com>
2025-03-12 00:18:20 +0200

    Add project files.

---
+++ First.Demo/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using First.Demo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace First.Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        //Model Binding 
+        // Action Methodm => public Non-Static method
+
+        //From route => Home/Index/10
+        //From Querystring => Home/Index?id=10&name=ahmed
+        //From Body => 
+        //From form 
+        //From Header 
+        public string Index([FromQuery]int id,string name, Product product)
+        {
+            return $"id = {id} :: name = {name} product=> id:{product.Id} :: name:{product.Name}";
+            //return $"Hello From Index In Home Controller With Id = {id}";
+        }
+    }
+}
---
+++ First.Demo/Program.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace First.Demo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var WebApplicationBuilder = WebApplication.CreateBuilder();
+
+            #region ConfigureServices
+            WebApplicationBuilder.Services.AddControllersWithViews(); 
+            #endregion
+
+            var app = WebApplicationBuilder.Build();
+            #region Configure
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseRouting();
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/", async context =>
+            //    {
+            //        await context.Response.WriteAsync("Hello World");
+
+            //    });
+            //    endpoints.MapGet("/Home", async context =>
+            //    {
+            //        await context.Response.WriteAsync("Hello From Home Page!");
+            //    });
+            //    endpoints.MapControllerRoute(
+            //        name: "default",
+            //        pattern: "{Controller=Home}/{Action=Index}/{id?}"
+            //        //defaults: new { controller = "Home", Action = "Index" },
+            //        //constraints: new { id = new IntRouteConstraint() }//old And Not Used
+            //        );
+            app.MapGet("/", async context =>
+            {
+                await context.Response.WriteAsync("Hello World");
+
+            });
+            app.MapGet("/Home", async context =>
+            {
+                await context.Response.WriteAsync("Hello From Home Page!");
+            });
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{Controller=Home}/{Action=Index}/{id?}"
+                //defaults: new { controller = "Home", Action = "Index" },
+                //constraints: new { id = new IntRouteConstraint() }//old And Not Used
+                );
+            #endregion
+
+            app.Run();
+
+        }
+
+        //public static IHostBuilder CreateHostBuilder(string[] args) =>
+        //    Host.CreateDefaultBuilder(args)
+        //        .ConfigureWebHostDefaults(webBuilder =>
+        //        {
+        //            webBuilder.UseStartup<Startup>();
+        //        });
+    }
+}
